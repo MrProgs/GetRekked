cf7f19b54692ac3a21ba15e5e0975eb1b280485d
Sean Daly <sbmdaly92@yahoo.com>
2014-05-23 12:05:50 -0400

    Some more changes

--- GetRekked/src/edu/scranton/getrekked/shared/BookReviewDao.java
+++ GetRekked/src/edu/scranton/getrekked/shared/BookReviewDao.java
@@ -1,5 +1,37 @@
 package edu.scranton.getrekked.shared;
 
-public class BookReviewDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import edu.scranton.getrekked.database.Connection.DatabaseHelper;
+
+public class BookReviewDao {
+	private Connection mConnection = null;
+    private DatabaseHelper mDb;
+    
+    public BookReviewDao(){
+    	String host = "134.198.169.32";
+        String dbName = "dbName";
+        String username = "username";
+        String password = "password";
+        
+        this.mDb = new DatabaseHelper(host, dbName, username, password);
+        this.mConnection = this.mDb.getConnection();
+    }
+    
+    public void close() {
+        this.mDb.close();
+   }
+    
+    public void insertBookReview(BookReview review){
+    	insertBookReviewInfo(review);
+    }
+    
+    public void insertBookReviewInfo(BookReview review){
+    	String sql = "INSERT INTO book_review VALUES(?, ?, ?, ?)";
+    }
 }
